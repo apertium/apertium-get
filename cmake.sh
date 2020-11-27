ea2424ce4f8ef98b37d3005b8ec60a2a3c4184f6
Tino Didriksen <mail@tinodidriksen.com>
2020-11-27 09:26:41 +0100

    Add autoconf-style helpers; Add authors

---
+++ cmake.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+args=()
+
+while [[ $# > 0 ]];
+do
+	case "$1" in
+	--prefix)
+		args+=("-DCMAKE_INSTALL_PREFIX=$2")
+		shift 2
+		;;
+	--prefix=*)
+		args+=("-DCMAKE_INSTALL_PREFIX=${1#*=}")
+		shift
+		;;
+	*)
+		args+=("$1")
+		shift
+		;;
+	esac
+done
+
+set -- "${args[@]}"
+
+D=$(dirname "$0")
+
+echo "- rm -rf CMake caches"
+rm -rf install_manifest.txt CMakeCache.txt *.cmake CMakeFiles [sp]*/CMakeFiles [sp]*/*.cmake _CPack_Packages Testing test/T_*/grammar.cg3b
+echo "- cmake " "$@" "$D"
+cmake "$@" "$D"
+echo "- You may now perform: make -j3"
